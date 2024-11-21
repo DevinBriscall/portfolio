8124d3dbe999ebdab8c71f9b7bf880263114e833
DevinBriscall <devinbriscall@gmail.com>
2024-11-21 18:45:58 -0500

    remove theme switch from lincolnguessr interface

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -37,9 +37,11 @@ export default function App({ Component, pageProps }) {
 						/>
 					)}
 					<Component {...pageProps} />
-					<div className="fixed right-8 bottom-8 bg-inherit w-10 h-10 text-2xl hover:cursor-pointer flex justify-center items-center rounded-full hover:scale-125 transistion-all duration-300">
-						<ThemeSwitch />
-					</div>
+					{router.pathname != "/lincolnguessr" && (
+						<div className="fixed right-8 bottom-8 bg-inherit w-10 h-10 text-2xl hover:cursor-pointer flex justify-center items-center rounded-full hover:scale-125 transistion-all duration-300">
+							<ThemeSwitch />
+						</div>
+					)}
 				</Providers>
 			</main>
 		</>
