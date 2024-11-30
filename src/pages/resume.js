4a772413770a3ed4589979c0b4af40e2f5026784
DevinBriscall <devinbriscall@gmail.com>
2024-11-30 11:06:26 -0500

    add click here if resume doesn't load

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -4,7 +4,18 @@ export default function resume() {
 	return (
 		<>
 			<div className="h-screenMinusNavbar">
-				<div className="pdf-viewer h-screenMinusNavbar">
+				<p className="text-center">
+					If the resume does not load automatically,{" "}
+					<a
+						href="/docs/resume.pdf"
+						target="_blank"
+						className="text-blue-500 underline"
+					>
+						click here
+					</a>{" "}
+					to view it.
+				</p>
+				<div className="pdf-viewer h-full">
 					<iframe src="/docs/resume.pdf" className="h-full w-full" />
 				</div>
 			</div>
