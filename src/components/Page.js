6112b4c326eb2bcd52884be180279a8c31a94ce3
DevinBriscall <devinbriscall@gmail.com>
2024-12-04 13:27:45 -0500

    fix centering on 2k monitor

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,8 +2,10 @@ import React from "react";
 
 export default function Page({ noPadding = false, children }) {
 	return (
-		<div className={`${!noPadding && "px-2 sm:px-8"} max-w-[1920px]`}>
-			{children}
+		<div
+			className={`w-full flex justify-center ${!noPadding && "px-2 sm:px-8"}`}
+		>
+			<div className={` max-w-[1920px]`}>{children}</div>
 		</div>
 	);
 }
